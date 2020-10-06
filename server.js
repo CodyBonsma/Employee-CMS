2aabc092084bd2767121586395bf0fcd5453754a
Cody Bonsma <codybonsma@gmail.com>
2020-10-06 19:12:56 -0400

    ran the first function and queried some data.

--- server.js
+++ server.js
@@ -20,64 +20,95 @@ function init() {
   //   if (err) throw err;
   //   console.table(data);
   // });
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "whatDoYouWant",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-      ],
-    },
-  ]).then(function(response){
-    console.log(response);
-    if(response.whatDoYouWant === "View All Employees"){
-      viewAllEmployees();
-    } else if (response.whatDoYouWant === "View All Employees By Department"){
-      viewEmployeesByDept();
-    } else if(response.whatDoYouWant ===  "View All Employees By Manager"){
-      viewEmployeesByManager();
-    } else if(response.whatDoYouWant ===  "Add Employee"){
-      addEmployee();
-    } else if(response.whatDoYouWant ===  "Remove Employee"){
-      removeEmployee();
-    } else if(response.whatDoYouWant ===  "Update Employee Role"){
-      updateEmployee();
-    } else if(response.whatDoYouWant ===  "Update Employee Manager"){
-      updateEmployeeManager();
-    }
-  }).catch((err) =>{
-    if (err) throw err;
-  })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "whatDoYouWant",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+        ],
+      },
+    ])
+    .then(function (response) {
+      console.log(response);
+      if (response.whatDoYouWant === "View All Employees") {
+        viewAllEmployees();
+      } else if (
+        response.whatDoYouWant === "View All Employees By Department"
+      ) {
+        viewEmployeesByDept();
+      } else if (response.whatDoYouWant === "View All Employees By Manager") {
+        viewEmployeesByManager();
+      } else if (response.whatDoYouWant === "Add Employee") {
+        addEmployee();
+      } else if (response.whatDoYouWant === "Remove Employee") {
+        removeEmployee();
+      } else if (response.whatDoYouWant === "Update Employee Role") {
+        updateEmployee();
+      } else if (response.whatDoYouWant === "Update Employee Manager") {
+        updateEmployeeManager();
+      }
+    })
+    .catch((err) => {
+      if (err) throw err;
+    });
 }
 
+// related functions from the init prompt
+//=======================================
+
 // function for viewing all employees
-function viewAllEmployees(){
- console.log("view all employees");
+function viewAllEmployees() {
+  console.log("view all employees");
+  connection.query("SELECT * FROM employee", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+  });
 }
-// function for viewing all employees by department 
-function viewEmployeesByDept(){
+
+// function for viewing all employees by department
+function viewEmployeesByDept() {
   console.log("view all employees by department");
+  connection.query(`SELECT employee.first_name, employee.last_name, comp_role.title, department.name, comp_role.salary FROM comp_role
+  INNER JOIN employee ON employee.role_id = role_id
+  INNER JOIN department ON comp_role.department_id = department.id`, (err, data) =>{
+    if (err) throw err;
+    console.table(data);
+  });
 }
+
 // function for viewing all employees by Manager
-function viewEmployeesByManager(){
+function viewEmployeesByManager() {
   console.log("view all employees by Manager");
 }
+
 // function for adding employees
-function addEmployee(){
+function addEmployee() {
   console.log("Add employee function");
 }
 
-function removeEmployee(){
+// function for removing employees
+function removeEmployee() {
   console.log("Remove an employee");
 }
 
+// function for updating employee
+function updateEmployee() {
+  console.log("Update Employee");
+}
+
+// update employee manager
+function updateEmployeeManager() {
+  console.log("Update Employee manager");
+}
 // connect with all the required packages
 
 // create the schema database with the three tables (dept, role and employee)
