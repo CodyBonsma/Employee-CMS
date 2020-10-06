8e2f7ef367216ac818764cff7b42095296dabf8c
Cody Bonsma <codybonsma@gmail.com>
2020-10-05 22:33:04 -0400

    successfully connected with the database and retrieved data

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "DonaldDuck50!",
+  database: "employee_tracker_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  init();
+});
+
+function init() {
+  connection.query("SELECT * FROM comp_role", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+  });
+}
+
 // connect with all the required packages
 
 // create the schema database with the three tables (dept, role and employee)
@@ -5,7 +29,7 @@
 // setup connection with the server
 
 // with inquirer, let users create (dept, role and employee)
-// these changes should then update the database 
+// these changes should then update the database
 // ==================================
 // ===== What would you like to do?
 //View all employees
@@ -14,6 +38,6 @@
 //Add employee
 //Remove employee
 //Update employee role
-//Update employee manager 
+//Update employee manager
 
-// create functionality for users to view the the tables
+// create functionality
