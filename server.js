dad6714e2f9b786c75c03e719f571ca71b481eaa
Cody Bonsma <codybonsma@gmail.com>
2020-10-06 18:18:58 -0400

    created the init prompt to ask the user what they'd like to do. added the related functions

--- server.js
+++ server.js
@@ -16,10 +16,66 @@ connection.connect((err) => {
 });
 
 function init() {
-  connection.query("SELECT * FROM comp_role", (err, data) => {
+  // connection.query("SELECT * FROM employee", (err, data) => {
+  //   if (err) throw err;
+  //   console.table(data);
+  // });
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "whatDoYouWant",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    },
+  ]).then(function(response){
+    console.log(response);
+    if(response.whatDoYouWant === "View All Employees"){
+      viewAllEmployees();
+    } else if (response.whatDoYouWant === "View All Employees By Department"){
+      viewEmployeesByDept();
+    } else if(response.whatDoYouWant ===  "View All Employees By Manager"){
+      viewEmployeesByManager();
+    } else if(response.whatDoYouWant ===  "Add Employee"){
+      addEmployee();
+    } else if(response.whatDoYouWant ===  "Remove Employee"){
+      removeEmployee();
+    } else if(response.whatDoYouWant ===  "Update Employee Role"){
+      updateEmployee();
+    } else if(response.whatDoYouWant ===  "Update Employee Manager"){
+      updateEmployeeManager();
+    }
+  }).catch((err) =>{
     if (err) throw err;
-    console.table(data);
-  });
+  })
+}
+
+// function for viewing all employees
+function viewAllEmployees(){
+ console.log("view all employees");
+}
+// function for viewing all employees by department 
+function viewEmployeesByDept(){
+  console.log("view all employees by department");
+}
+// function for viewing all employees by Manager
+function viewEmployeesByManager(){
+  console.log("view all employees by Manager");
+}
+// function for adding employees
+function addEmployee(){
+  console.log("Add employee function");
+}
+
+function removeEmployee(){
+  console.log("Remove an employee");
 }
 
 // connect with all the required packages
