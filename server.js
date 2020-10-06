e342b137cf417173fa614480295faf1e1bb4f887
Cody Bonsma <codybonsma@gmail.com>
2020-10-05 21:59:00 -0400

    created the file, installed necessary packages and began pseudo coding

---
+++ server.js
@@ -0,0 +1,19 @@
+// connect with all the required packages
+
+// create the schema database with the three tables (dept, role and employee)
+
+// setup connection with the server
+
+// with inquirer, let users create (dept, role and employee)
+// these changes should then update the database 
+// ==================================
+// ===== What would you like to do?
+//View all employees
+//View all employees by Dept
+//View all employees by Manager
+//Add employee
+//Remove employee
+//Update employee role
+//Update employee manager 
+
+// create functionality for users to view the the tables
