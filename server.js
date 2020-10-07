7060fd69e9807a7287986c8bb45e99916873a59b
Cody Bonsma <codybonsma@gmail.com>
2020-10-06 23:24:47 -0400

    added view and add functions

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ connection.connect((err) => {
 });
 
 function init() {
-  // connection.query("SELECT * FROM employee", (err, data) => {
-  //   if (err) throw err;
-  //   console.table(data);
-  // });
   inquirer
     .prompt([
       {
@@ -31,8 +27,11 @@ function init() {
           "View All Employees By Department",
           "View All Employees By Manager",
           "Add Employee",
+          "Add Department",
+          "Add Role",
+          "View Roles",
+          "View Departments",
           "Remove Employee",
-          "Update Employee Role",
           "Update Employee Manager",
         ],
       },
@@ -51,10 +50,16 @@ function init() {
         addEmployee();
       } else if (response.whatDoYouWant === "Remove Employee") {
         removeEmployee();
-      } else if (response.whatDoYouWant === "Update Employee Role") {
-        updateEmployee();
+      } else if (response.whatDoYouWant === "View Departments") {
+        viewDepartment();
       } else if (response.whatDoYouWant === "Update Employee Manager") {
         updateEmployeeManager();
+      } else if (response.whatDoYouWant === "Add Department") {
+        addDept();
+      } else if (response.whatDoYouWant === "View Roles") {
+        viewRoles();
+      } else if (response.whatDoYouWant === "Add Role") {
+        addRole();
       }
     })
     .catch((err) => {
@@ -65,39 +70,139 @@ function init() {
 // related functions from the init prompt
 //=======================================
 
+const currentEmployees = [];
+
 // function for viewing all employees
 function viewAllEmployees() {
   console.log("view all employees");
   connection.query("SELECT * FROM employee", (err, data) => {
     if (err) throw err;
     console.table(data);
+    currentEmployees.push(data);
+  });
+}
+
+function viewRoles(){
+  connection.query("SELECT * FROM comp_role", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    init();
   });
 }
 
 // function for viewing all employees by department
 function viewEmployeesByDept() {
   console.log("view all employees by department");
-  connection.query(`SELECT employee.first_name, employee.last_name, comp_role.title, department.name, comp_role.salary FROM comp_role
-  INNER JOIN employee ON employee.role_id = role_id
-  INNER JOIN department ON comp_role.department_id = department.id`, (err, data) =>{
-    if (err) throw err;
-    console.table(data);
-  });
+  connection.query(
+    `SELECT employee.first_name, employee.last_name, comp_role.title, department.name, comp_role.salary FROM comp_role
+  INNER JOIN employee ON employee.role_id = comp_role.id
+  INNER JOIN department ON comp_role.department_id = department.id`,
+    (err, data) => {
+      if (err) throw err;
+      console.table(data);
+    }
+  );
 }
 
+function viewDepartment(){
+  connection.query("SELECT * FROM department", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    init();
+  });
+}
 // function for viewing all employees by Manager
-function viewEmployeesByManager() {
-  console.log("view all employees by Manager");
+// function viewEmployeesByManager() {
+//   console.log("view all employees by Manager");
+// }
+
+function addDept(){
+console.log("Ready to add a department");
+inquirer.prompt([
+  {
+    type: "input",
+    name: "addDepartment",
+    message: "What department would you like to add?"
+  }
+]).then(function(result){
+  connection.query(`INSERT INTO department (name) VALUES (?)`, [result.addDepartment], (err,data) =>{
+    if(err) throw err;
+    console.table(data);
+    init();
+  })
+})
 }
 
 // function for adding employees
 function addEmployee() {
   console.log("Add employee function");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter employee's first name",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter employee's last name",
+      },
+      {
+        type: "number",
+        name: "employeeId",
+        message: "Please enter employee's ID",
+      },
+      {
+        type: "list",
+        name: "employeeManager",
+        message: "Please select the employee's Manager",
+        choices: [1, 2, 3],
+      },
+    ])
+    .then(function (result) {
+      connection.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?) `,
+        [
+          [
+            result.firstName,
+            result.lastName,
+            parseInt(result.employeeId),
+            result.employeeManager,
+          ],
+        ],
+        (err, data) => {
+          if (err) throw err;
+          // console.log(data);
+          console.table(data);
+          init();
+        }
+      );
+    })
+    .catch((err) => {
+      if (err) throw err;
+    });
 }
 
 // function for removing employees
 function removeEmployee() {
   console.log("Remove an employee");
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "deleteEmployee",
+      message: "Which employee would you like to delete",
+      choices: currentView
+    }
+  ]).then(function(result){
+    connection.query(`DELETE FROM employee WHERE first_name = ?`, [currentEmployees], function(err, data){
+      if (err) throw err;
+      console.log(data);
+      init();
+    })
+  }).catch((err) => {
+    if (err) throw err;
+  })
 }
 
 // function for updating employee
